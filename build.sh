bb5a77a7c55629ea6261305533cc3d4077d2c46d
chromosomaly <26923860+chromosomaly@users.noreply.github.com>
2022-03-06 01:43:56 +0800

    fix: force changes to make it easier in local env

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ source vars
 DOCKER=$(which docker)
 BUILD_DATE=$(date -u '+%Y-%m-%dT%H:%M:%S%z')
 
-# build image
-$DOCKER build --pull                               \
+docker build --pull                               \
               --tag ${IMAGE_NAME}                  \
               --build-arg BUILD_DATE=${BUILD_DATE} \
               .
